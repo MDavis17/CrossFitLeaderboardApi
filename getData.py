e7ad91e62f502489df5cc4e54226502d1df66f2d
Max Davis <madmax.davis@gmail.com>
2021-04-26 23:40:25 -0600

    updated getData to pull specific data from leaderboard data. additional option validation added. still only pulling 2 pages.

--- getData.py
+++ getData.py
@@ -6,33 +6,39 @@ def getUrl(competition,year,eventNum,pageNum):
     return "https://games.crossfit.com/competitions/api/v1/competitions/" + competition + "/" + str(year) + "/leaderboards?&sort=" + str(eventNum) + "&page=" + str(pageNum)
 
 def getData(competition,year,eventNum):
+    allData = []
     data = json.loads(requests.get(getUrl(competition,year,eventNum,1)).text)
     pageTotal = data["pagination"]["totalPages"]
     for i in range(1,3): # for now, just pages 1 and 2. fix with int(pageTotal+1)
         page = json.loads(requests.get(getUrl(competition,year,eventNum,i)).text)
-        data.update(page)
-    return data
-
-def getTotalPageCount(competition,year,eventNum,pageNum):
-    data = json.loads(requests.get(getUrl(competition,year,eventNum,pageNum)).text)
-    return data["pagination"]["totalPages"]
+        leaderboardRows = page["leaderboardRows"]
+        for row in leaderboardRows:
+            rowData = {}
+            rowData["entrant"] = row["entrant"]
+            rowData["overallRank"] = row["overallRank"]
+            rowData["overallScore"] = row["overallScore"]
+            allData.append(rowData)
+    return allData
 
 def main(argv):
     comp = ''
     year = ''
     eventnum = 0
     try:
-        opts, args = getopt.getopt(argv,"hc:y:e",["comp=","year=","eventnum="])
+        opts, args = getopt.getopt(argv,"hc:y:e:",["comp=","year=","eventnum="])
     except getopt.GetoptError:
-        print 'getData.py -c <competition> -y <year> -e <eventnum>'
+        print 'getData.py -c <competition> -y <year> optional: -e <event num>'
         sys.exit(2)
     optList = []
     for opt, arg in opts:
         optList.append(opt)
         if opt == '-h':
-            print 'getData.py -c <competition> -y <year> -e <eventnum>'
+            print 'getData.py -c <competition> -y <year> optional: -e <event num>'
             sys.exit()
         elif opt in ("-c", "--comp"):
+            if arg not in ["open","regionals","games"]:
+                print 'invalid competition argument (open, regionals, games)'
+                sys.exit(2)
             comp = arg
         elif opt in ("-y", "--year"):
             year = arg
@@ -44,8 +50,19 @@ def main(argv):
     if '-y' not in optList:
         print '-y <year> is a required option'
         sys.exit(2)
-    dataFile = open(str(comp)+"_"+str(year)+".txt","w")
-    dataFile.write(str(getData(str(comp),year,int(eventnum))))
+    if comp == "open" and int(year) < 2017 or int(year) > 2020:
+        print 'invalid year for the open (valid for 2017-2020)'
+        sys.exit(2)
+    if comp == "regionals" and int(year) != 2017:
+        print 'invalid year for regionals (2017 only)'
+        sys.exit(2)
+    if comp == "games" and int(year) < 2007 or int(year) > 2020:
+        print 'invalid year for the open (valid for 2007-2020)'
+        sys.exit(2)
+
+    dataFile = open(str(comp)+"_"+str(year)+".json","w")
+    data = getData(str(comp),year,int(eventnum))
+    dataFile.write(str(data))
     dataFile.close()
 
 if __name__ == "__main__":
